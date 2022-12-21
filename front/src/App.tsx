7d645112a43662dbefd0474b8c4b75dcecaa18c7
LEEHYUNBOK <gusqhrzkq@naver.com>
2022-12-21 16:49:35 +0900

    Feat : Main title 생성

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css'
 import Main from './pages/main'
-
+import MarkDownAdd from './pages/markdownAdd';
 function App() {
   return (
     <>
       <Main/>
+      <MarkDownAdd/>
     </>
     );
 }
--- front/src/pages/markdownAdd.tsx
+++ front/src/pages/markdownAdd.tsx
@@ -1,5 +1,19 @@
+import React, { useState } from 'react'
+import MDEditor from "@uiw/react-md-editor";
+import styled from "@emotion/styled";
+
 function MarkDownAdd() {
-    
+    const [value, setValue] = useState<string>();
+    return (
+    <MDArea>
+      <MDEditor height={`70vh`} highlightEnable={false} value={value} onChange={setValue}/>
+    </MDArea>
+    )
 }
 
 export default MarkDownAdd
+
+const MDArea = styled.div`
+    width: 90%;
+    margin: 0px auto;
+` 
