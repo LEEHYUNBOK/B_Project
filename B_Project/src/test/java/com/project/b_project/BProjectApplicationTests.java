86e98b70127671d66509dbefe8ada0f52ace5dd4
LEEHYUNBOK <gusqhrzkq@naver.com>
2022-12-21 15:17:22 +0900

    Feat : BackEnd setting

---
+++ B_Project/src/test/java/com/project/b_project/BProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.b_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
