1c9a51137fc6da07982b8998783cd9993f020210
LEEHYUNBOK <gusqhrzkq@naver.com>
2022-12-29 12:59:57 +0900

    Feat : abbB folder create

---
+++ front/src/pages/addBlog/markdownAdd.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import MDEditor from "@uiw/react-md-editor";
+import styled from "@emotion/styled";
+
+
+// 참고 EASYME.md github
+function MarkDownAdd() {
+    const [value, setValue] = useState<string>();
+    return (
+    <MDArea>
+      <MDEditor height={`70vh`} highlightEnable={false} value={value} onChange={setValue}/>
+    </MDArea>
+    )
+}
+
+export default MarkDownAdd
+
+const MDArea = styled.div`
+    width: 90%;
+    margin: 0px auto;
+` 
